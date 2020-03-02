d159ef36e8c8d00aab5ed60f4ea6ae9fd083237c
gbrattsev <gbrattsev@gmail.com>
2020-03-02 18:52:14 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -194,6 +194,40 @@ window.addEventListener('DOMContentLoaded', function() {
   let slideIndex = 1,
       slides = document.querySelectorAll('.slider-item'),
       prev = document.querySelector('.prev'),
-      next = document.querySelector('.next');
+      next = document.querySelector('.next'),
+      dotsWrap = document.querySelector('.slider-dots'),
+      dots = document.querySelectorAll('.dot');
+
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    slides.forEach((item) => {item.style.display = 'none'});
+    dots.forEach((item) => {item.classList.remove('dot-active')});
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+  }
+
+  function plusSlide(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  prev.addEventListener('click', () => {plusSlide(-1)});
+
+  next.addEventListener('click', () => {plusSlide(1)});
+
+  dotsWrap.addEventListener('click', function(e) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (e.target == dots[i -1] && e.target.classList.contains('dot')) {
+        currentSlide(i);
+      }
+    }
+  });
 
 });
